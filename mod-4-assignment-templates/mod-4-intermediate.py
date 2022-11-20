b70fdeae062fbb9cb47ef10cbf0d68a2364d60f1
Koji Lim <krslim03@gmail.com>
2022-11-21 02:51:09 +1100

    if to elif

--- mod-4-assignment-templates/mod-4-intermediate.py
+++ mod-4-assignment-templates/mod-4-intermediate.py
@@ -19,7 +19,7 @@ def shift_letter(letter, shift):
 
         return ltr_list[0]
 
-    if letter == " ":  # conditional if " " is the input
+    elif letter == " ":  # conditional if " " is the input
         return " "
 
 def caesar_cipher(message, shift):
@@ -35,7 +35,11 @@ def caesar_cipher(message, shift):
 
 def shift_by_letter(letter, letter_shift):
     ltr_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    if letter == ltr_list:
+    ltr_index = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25']
+    
+    if letter in ltr_list:
+    
+
         
 
     '''Shift By Letter. 
