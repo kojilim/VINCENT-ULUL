2824542ee1607c6ab6aca743be11288c208b893c
Koji Lim <krslim03@gmail.com>
2022-12-09 03:27:39 +0800

    advanced push

--- mod-4-assignment-templates/mod-4-advanced.py
+++ mod-4-assignment-templates/mod-4-advanced.py
@@ -1,106 +1,75 @@
-'''Module 4: Individual Programming Assignment 1
-
-Parsing Data
-
-This assignment covers your ability to manipulate data in Python.
-'''
-
 def relationship_status(from_member, to_member, social_graph):
-    '''Relationship Status.
-    15 points.
-
-    Let us pretend that you are building a new app.
-    Your app supports social media functionality, which means that users can have
-    relationships with other users.
-
-    There are two guidelines for describing relationships on this social media app:
-    1. Any user can follow any other user.
-    2. If two users follow each other, they are considered friends.
-
-    This function describes the relationship that two users have with each other.
-
-    Please see "assignment-4-sample-data.py" for sample data. The social graph
-    will adhere to the same pattern.
-
-    Parameters
-    ----------
-    from_member: str
-        the subject member
-    to_member: str
-        the object member
-    social_graph: dict
-        the relationship data    
-
-    Returns
-    -------
-    str
-        "follower" if fromMember follows toMember,
-        "followed by" if fromMember is followed by toMember,
-        "friends" if fromMember and toMember follow each other,
-        "no relationship" if neither fromMember nor toMember follow each other.
-    '''
-    # Replace `pass` with your code. 
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    fromFollow = social_graph[from_member]['following']
+    toFollow = social_graph[to_member]['following']
+    if from_member in toFollow and to_member in fromFollow:
+        relationship = 'friends'
+    elif from_member in toFollow:
+        relationship = 'followed by'
+    elif to_member in fromFollow:
+        relationship = 'follower'
+    else:
+        relationship = 'no relationship'
+    return (str(relationship))
 
 
+import numpy as np
 def tic_tac_toe(board):
-    '''Tic Tac Toe. 
-    15 points.
+    SIZE = len(board)
 
-    Tic Tac Toe is a common paper-and-pencil game. 
-    Players must attempt to successfully draw a straight line of their symbol across a grid.
-    The player that does this first is considered the winner.
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if board[i][j] == "X":
+                board[i][j] = 1
+            elif board[i][j] == "O":
+                board[i][j] = -1
+            else:
+                board[i][j] = 0
 
-    This function evaluates a tic tac toe board and returns the winner.
+    board = np.array(board)
+    col_sums = np.sum(board, axis=1)
 
-    Please see "assignment-4-sample-data.py" for sample data. The board will adhere
-    to the same pattern. The board may by 3x3, 4x4, 5x5, or 6x6. The board will never
-    have more than one winner. The board will only ever have 2 unique symbols at the same time.
+    #Horizontal check
+    for col_sum in col_sums:
+        if col_sum == SIZE:
+            return "X"
+        elif col_sum == -SIZE:
+            return "O"
 
-    Parameters
-    ----------
-    board: list
-        the representation of the tic-tac-toe board as a square list of lists
+    #Vertical check
+    row_sums = np.sum(board, axis=0)
+    for row_sum in row_sums:
+        if row_sum == SIZE:
+            return "X"
+        elif row_sum == -SIZE:
+            return "O"
 
-    Returns
-    -------
-    str
-        the symbol of the winner or "NO WINNER" if there is no winner
-    '''
-    # Replace `pass` with your code. 
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    #Diagonal check
+    primary_diag_sum = 0  # going down to the right diagonal
+    for i in range(SIZE):
+        primary_diag_sum += board[i][i]
+    if primary_diag_sum == SIZE:
+        return "X"
+    elif primary_diag_sum == -SIZE:
+        return "O"
+
+    #Inverse diagonal check
+    secondary_diag_sum = 0
+    for i in range(SIZE):
+        secondary_diag_sum += board[i, (SIZE - 1) - i]
+    if secondary_diag_sum == SIZE:
+        return "X"
+    elif secondary_diag_sum == -SIZE:
+        return "O"
+    else:
+        return "No Winner"
 
 def eta(first_stop, second_stop, route_map):
-    '''ETA. 
-    20 points.
+    parameters = (first_stop, second_stop)
+    travelTime = 0
 
-    A shuttle van service is tasked to travel along a predefined circlar route.
-    This route is divided into several legs between stops.
-    The route is one-way only, and it is fully connected to itself.
+    for i in route_map:
+        if parameters == i:
+            travelTime += route_map[i]['travel_time_mins']
+            break
 
-    This function returns how long it will take the shuttle to arrive at a stop
-    after leaving another stop.
-
-    Please see the sample data file in this same folder for sample data. The route map will
-    adhere to the same pattern. The route map may contain more legs and more stops,
-    but it will always be one-way and fully enclosed.
-
-    Parameters
-    ----------
-    first_stop: str
-        the stop that the shuttle will leave
-    second_stop: str
-        the stop that the shuttle will arrive at
-    route_map: dict
-        the data describing the routes
-
-    Returns
-    -------
-    int
-        the time it will take the shuttle to travel from first_stop to second_stop
-    '''
-    # Replace `pass` with your code. 
-    # Stay within the function. Only use the parameters as input. The function should return your answer.
-    pass
+    return (travelTime)
