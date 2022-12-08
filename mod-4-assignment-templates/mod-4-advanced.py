e205a7296640c56aff5d793c0d5e1e41fa15e0b7
Koji Lim <krslim03@gmail.com>
2022-12-09 03:30:37 +0800

    edit var

--- mod-4-assignment-templates/mod-4-advanced.py
+++ mod-4-assignment-templates/mod-4-advanced.py
@@ -1,11 +1,11 @@
 def relationship_status(from_member, to_member, social_graph):
-    fromFollow = social_graph[from_member]['following']
-    toFollow = social_graph[to_member]['following']
-    if from_member in toFollow and to_member in fromFollow:
+    x = social_graph[from_member]['following']
+    y = social_graph[to_member]['following']
+    if from_member in y and to_member in x:
         relationship = 'friends'
-    elif from_member in toFollow:
+    elif from_member in y:
         relationship = 'followed by'
-    elif to_member in fromFollow:
+    elif to_member in x:
         relationship = 'follower'
     else:
         relationship = 'no relationship'
@@ -26,50 +26,50 @@ def tic_tac_toe(board):
                 board[i][j] = 0
 
     board = np.array(board)
-    col_sums = np.sum(board, axis=1)
+    cs = np.sum(board, axis=1)
 
     #Horizontal check
-    for col_sum in col_sums:
+    for col_sum in cs:
         if col_sum == SIZE:
             return "X"
         elif col_sum == -SIZE:
             return "O"
 
     #Vertical check
-    row_sums = np.sum(board, axis=0)
-    for row_sum in row_sums:
+    rs = np.sum(board, axis=0)
+    for row_sum in rs:
         if row_sum == SIZE:
             return "X"
         elif row_sum == -SIZE:
             return "O"
 
     #Diagonal check
-    primary_diag_sum = 0  # going down to the right diagonal
+    pds = 0  # going down to the right diagonal
     for i in range(SIZE):
-        primary_diag_sum += board[i][i]
-    if primary_diag_sum == SIZE:
+        pds += board[i][i]
+    if pds == SIZE:
         return "X"
-    elif primary_diag_sum == -SIZE:
+    elif pds == -SIZE:
         return "O"
 
     #Inverse diagonal check
-    secondary_diag_sum = 0
+    sds = 0
     for i in range(SIZE):
-        secondary_diag_sum += board[i, (SIZE - 1) - i]
-    if secondary_diag_sum == SIZE:
+        sds += board[i, (SIZE - 1) - i]
+    if sds == SIZE:
         return "X"
-    elif secondary_diag_sum == -SIZE:
+    elif sds == -SIZE:
         return "O"
     else:
         return "No Winner"
 
 def eta(first_stop, second_stop, route_map):
-    parameters = (first_stop, second_stop)
-    travelTime = 0
+    par = (first_stop, second_stop)
+    tt = 0
 
     for i in route_map:
-        if parameters == i:
-            travelTime += route_map[i]['travel_time_mins']
+        if par == i:
+            tt += route_map[i]['travel_time_mins']
             break
 
-    return (travelTime)
+    return (tt)
